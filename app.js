18e1615a03c9295a77adb223a642e6921a669834
Deepikaaradhya <86877676+Deepikaaradhya@users.noreply.github.com>
2021-11-06 10:30:06 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 const app = express();
-const port = 9001;
+const port = process.env.PORT||9001;
 const mongo = require('mongodb');
 const MongoClient = mongo.MongoClient;
 //const mongourl = "mongodb://localhost:27017"
